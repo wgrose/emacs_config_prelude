ee29806a3640fcaf58c26ab76f43516ecc47a9a2
William Grose <wgrose@wgrose.lax.corp.google.com>
2017-11-15 11:38:24 -0800

    c

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -4,3 +4,8 @@ rm ~/.emacs.d/prelude-modules.el
 ln -s $SCRIPT_PATH/prelude-modules.el ~/.emacs.d/prelude-modules.el
 rm -f ~/.emacs.d/personal
 ln -s $SCRIPT_PATH/personal/ ~/.emacs.d/personal
+
+profile_file="$HOME/.bashrc"
+if ! grep -q '.emacs.d/personal/init_bash.sh' "${profile_file}" ; then
+  echo "source \"$HOME/.emacs.d/personal/init_bash.sh\"" >> "${profile_file}"
+fi
