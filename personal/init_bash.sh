cd61ef38a8a00db5bcb435000478d39287f869f9
William Grose <wgrose@wgrose.c.googlers.com>
2017-12-22 13:36:51 -0500

    PS1 changes

--- personal/init_bash.sh
+++ personal/init_bash.sh
@@ -2,10 +2,8 @@ export EDITOR=emacsclient
 export HGMERGE=emacsclient
 
 source $HOME/.emacs.d/personal/include_citc.sh
-
+TRUNCATED_HOSTNAME=$(hostname | sed "s/\(\.corp\.google\.com\|\.googlers\.com\)//")
 if [ -f /etc/bash_completion ]; then
   source /etc/bash_completion;
-  PS1='\u@$(hostname | sed s/.corp.google.com//):\w$(__git_ps1 "(%s)")$(citc_current_switch_client_ps1 "(%s)")\$ ' ;
-else
-  PS1='\u@$(hostname | sed s/.corp.google.com//):\w$ ' ;
+  PS1='\u@${TRUNCATED_HOSTNAME}:\w$(__git_ps1 "(%s)")$(citc_current_switch_client_ps1 "(%s)")\$ ';
 fi
